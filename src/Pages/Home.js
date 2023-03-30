b6fc5498c09f565fc81068c269559f4fc7e856cd
Shuhaib Abdulla <shuhaib.mk300@gmail.com>
2023-03-30 20:36:23 +0530

    Header Added

---
+++ src/Pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Components/Header/Header';
+
+function Home(props) {
+  return (
+    <div className="homeParentDiv">
+    <Header />
+    {/* <Banner />
+    <Posts />
+    <Footer /> */}
+    </div>
+  )
+}
+
+export default Home;
