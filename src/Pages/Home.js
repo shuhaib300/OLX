7cd0e63453f335df2461338cbb1e535255d74faf
Shuhaib Abdulla <shuhaib.mk300@gmail.com>
2023-04-07 14:40:15 +0530

    Post Added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Banner from '../Components/Banner/Banner';
 import Header from '../Components/Header/Header';
+import Posts from '../Components/Posts/Post';
 
 function Home(props) {
   return (
     <div className="homeParentDiv">
     <Header/>
     <Banner/>
-    {/* <Posts />
-    <Footer /> */}
+    <Posts/>
+    {/* <Footer /> */}
     </div>
   )
 }
